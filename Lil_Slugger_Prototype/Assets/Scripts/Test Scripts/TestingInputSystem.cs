0ca13fcc145e8aa8cb3325f4b19d7f72e84cf623
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2024-04-11 15:38:34 +0100

    Testing Input System
    Testing Input System with basic jump.

--- Lil_Slugger_Prototype/Assets/Scripts/Test Scripts/TestingInputSystem.cs
+++ Lil_Slugger_Prototype/Assets/Scripts/Test Scripts/TestingInputSystem.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TestingInputSystem : MonoBehaviour
 {
-    public void Jump()
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+
+    private void Awake()
     {
-        Debug.Log("Jump");
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInput.onActionTriggered += PlayerInput_onActionTriggered;
+    }
+
+    private void PlayerInput_onActionTriggered(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump" + context.phase);
+        rb.AddForce(Vector3.up * 2, ForceMode.Impulse);
     }
 }
