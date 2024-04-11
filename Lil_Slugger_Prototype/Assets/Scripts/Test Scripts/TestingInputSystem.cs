979838f1b507ba1bcb1f66c94441d7d73102169c
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2024-04-11 16:29:18 +0100

    Testing Movement
    Testing Stick movement with Input System Manager.
    
    Need ContextCallback reference to use Movement() in fixed update.

--- Lil_Slugger_Prototype/Assets/Scripts/Test Scripts/TestingInputSystem.cs
+++ Lil_Slugger_Prototype/Assets/Scripts/Test Scripts/TestingInputSystem.cs
@@ -6,24 +6,32 @@ using UnityEngine.InputSystem;
 public class TestingInputSystem : MonoBehaviour
 {
     private Rigidbody rb;
-    private PlayerInput playerInput;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>();
-
-        playerInput.onActionTriggered += PlayerInput_onActionTriggered;
     }
 
-    private void PlayerInput_onActionTriggered(InputAction.CallbackContext context)
+    private void FixedUpdate()
+    {
+        //Movement();
+    }
+
+    public void Movement(InputAction.CallbackContext context)
     {
         Debug.Log(context);
+
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * 0.1f, ForceMode.Impulse);
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        Debug.Log("Jump" + context.phase);
-        rb.AddForce(Vector3.up * 2, ForceMode.Impulse);
+        Debug.Log(context);
+        if (context.performed)
+        {
+            Debug.Log("Jump" + context.phase);
+            rb.AddForce(Vector3.up * 3, ForceMode.Impulse);
+        }
     }
 }
